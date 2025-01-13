0ed5c25e1e3856829ca849bba8b38d5505161865
testName <k023g0068@g.neec.ac.jp>
2025-01-14 05:58:21 +0900

    長押し時のゲージを追加

--- Assets/Scripts/PacketScript.cs
+++ Assets/Scripts/PacketScript.cs
@@ -7,7 +7,7 @@ public class PacketScript : MonoBehaviour
 {
     [SerializeField] uint type;
     [SerializeField] uint zipped;
-    [SerializeField] float totalChargeTime;
+    [SerializeField] float totalChargeTime=0.1f;
     float curChargeTime;
 
     bool attack;
@@ -18,36 +18,48 @@ public class PacketScript : MonoBehaviour
     //UnityEngine.Color curColor;
     SpriteRenderer spriteRenderer;
 
+    GameObject gaugeObj;
+    float gaugeRatio;
+
     public uint GetTypes() { return type; }
     public uint GetZipped() { return zipped; }
     // Start is called before the first frame update
     void Start()
     {
-        spriteRenderer= gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         initColor = spriteRenderer.color;
         curChargeTime = totalChargeTime * (1 + zipped / 2);
+
+        gaugeObj = transform.GetChild(0).gameObject;
+        gaugeObj.transform.localScale = new Vector3(0, 1, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
         Charge();
+        gaugeRatio = (totalChargeTime - curChargeTime) / totalChargeTime;
+        gaugeObj.transform.localScale = new Vector3(Mathf.Clamp01(gaugeRatio), 1, 1);
+
     }
 
     void Charge()
     {
-        //if()ホールド中
-        curChargeTime-=Time.deltaTime;
+        if (!touch) { return; } //ホールド中
+        if (Input.GetMouseButton(0))
+        {
+            curChargeTime -= Time.deltaTime;
+        }
     }
 
-   
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "MousePointer")
         {
             touch = true;
 
-            spriteRenderer.color = new UnityEngine.Color(initColor.r + 0.2f, initColor.r + 0.2f, initColor.b+ 0.2f);
+            spriteRenderer.color = new UnityEngine.Color(initColor.r + 0.2f, initColor.r + 0.2f, initColor.b + 0.2f);
         }
     }
 
